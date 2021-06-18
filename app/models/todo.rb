e6b01d01c92665d0f4e38b12cfde26cc232bea5b
Arjun Patil <arjunshivaji100@gmail.com>
2021-06-18 16:44:56 +0530

    Render index

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,42 @@
 class Todo < ActiveRecord::Base
-  def to_pleasant_string
-    isCompleted = completed ? "[X]" : "[ ]"
-    "#{id} #{due_date.to_s(:long)} #{todo_text} #{isCompleted}"
+  def due_today?
+    due_date == Date.today
+  end
+
+  def to_displayable_string
+    display_status = completed ? "[X]" : "[ ]"
+    display_date = due_today? ? nil : due_date
+    "#{self.id}. #{display_status} #{todo_text} #{display_date}"
+  end
+
+  def self.overdue
+    all.where("due_date < ?", Date.today)
+  end
+
+  def self.due_today
+    all.where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date > ?", Date.today)
+  end
+
+  def self.completed
+    all.where(completed: true)
+  end
+
+  def self.to_displayable_list
+    all.map { |todo| todo.to_displayable_string }
+  end
+
+  def self.add_task(h)
+    create!(todo_text: h[:todo_text], due_date: (Date.today + h[:due_in_days]), completed: false)
+  end
+
+  def self.mark_as_complete(id)
+    todo = find(id)
+    todo.completed = true
+    todo.save
+    todo
   end
 end
