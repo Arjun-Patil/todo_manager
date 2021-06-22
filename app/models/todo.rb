6df8db44d73e74ca57f372e5734b20efc66c62b4
Arjun Patil <arjunshivaji100@gmail.com>
2021-06-23 04:47:35 +0530

    Remove unwanted methods

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,42 +1,21 @@
 class Todo < ActiveRecord::Base
-  def due_today?
-    due_date == Date.today
-  end
-
-  def to_displayable_string
-    display_status = completed ? "[X]" : "[ ]"
-    display_date = due_today? ? nil : due_date
-    "#{self.id}. #{display_status} #{todo_text} #{display_date}"
-  end
-
   def self.overdue
-    all.where("due_date < ?", Date.today)
+    order(:due_date).where("due_date < ?", Date.today)
   end
 
   def self.due_today
-    all.where("due_date = ?", Date.today)
+    order(:due_date).where("due_date = ?", Date.today)
   end
 
   def self.due_later
-    all.where("due_date > ?", Date.today)
+    order(:due_date).where("due_date > ?", Date.today)
   end
 
   def self.completed
-    all.where(completed: true)
+    order(:due_date).where(completed: true)
   end
 
   def self.uncompleted
-    all.where(completed: false)
-  end
-
-  def self.add_task(h)
-    create!(todo_text: h[:todo_text], due_date: (Date.today + h[:due_in_days]), completed: false)
-  end
-
-  def self.mark_as_complete(id)
-    todo = find(id)
-    todo.completed = true
-    todo.save
-    todo
+    order(:due_date).where(completed: false)
   end
 end
