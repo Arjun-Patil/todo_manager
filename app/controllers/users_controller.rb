918cb1dc6ffba23201e1850a29c9dc98b4cbe926
Arjun Patil <arjunshivaji100@gmail.com>
2021-06-25 17:50:19 +0530

    Use has_secure_password to store user password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,16 @@
 # users_controller.rb
 class UsersController < ApplicationController
-  skip_before_action :verify_authenticity_token
-
-  def index
-    render plain: User.all.map { |user| user.to_pleasant_string }.join("\n")
+  def new
+    render "new"
   end
 
   def create
-    name = params[:name]
-    email = params[:email]
-    password = params[:password]
-    new_user = User.create!(
-      name: name,
-      email: email,
-      password: password,
+    User.create!(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      password: params[:password],
     )
-
-    response_text = "New user is added with the id #{new_user.id}"
-    render plain: response_text
-  end
-
-  def login
-    email = params[:email]
-    password = params[:password]
-    users = User.where("email = ? and password = ?", email, password)
-    response = !users.empty?
-    render plain: response
-  end
-
-  def show
-    id = params[:id]
-    user = User.find(id)
-    render plain: user.to_pleasant_string
+    redirect_to "/"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,4 @@
 class User < ActiveRecord::Base
+  has_secure_password
   has_many :todos
-
-  def to_pleasant_string
-    "#{id} #{name} #{email} #{[password]}"
-  end
 end
