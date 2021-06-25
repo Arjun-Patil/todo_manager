d4d840c66bf0887cce07d370e8f2b460a73d835e
Arjun Patil <arjunshivaji100@gmail.com>
2021-06-25 19:49:55 +0530

    Add user signin functionality without session

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  # get "todos", to: "todos#index"
-  # post "todos/", to: "todos#create"
-  # get "todos/:id", to: "todos#show"
   get "/" => "home#index"
   resources :todos
-  post "users/login", to: "users#login"
   resources :users
+  get "/signin" => "sessions#new", as: :new_sessions
+  post "/signin" => "sessions#create", as: :sessions
 end
