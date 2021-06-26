3f500e5430d073a90b127a7e8777aacf605a57df
Arjun Patil <arjunshivaji100@gmail.com>
2021-06-26 18:43:52 +0530

    Render invalid attempt login message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,8 @@ class SessionsController < ApplicationController
       session[:current_user_id] = user.id
       redirect_to "/"
     else
-      render plain: "You have entered the incorrect password"
+      flash[:error] = "Your login attepmt was invalid. Please try again"
+      redirect_to new_sessions_path
     end
   end
 
